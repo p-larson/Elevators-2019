8a07985354c36ad708973a9627982613347e125b
Wowserman <wowserman.mc@gmail.com>
2019-07-12 14:56:15 -0500

    Clean up

---
+++ Elevators/Viewcontrollers & Storyboards/GameBuilder/LevelInfoEditorViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LevelInfoEditorViewController.swift
+//  Elevators
+//
+//  Created by Peter Larson on 7/12/19.
+//  Copyright © 2019 Peter Larson. All rights reserved.
+//
+
+import Foundation
